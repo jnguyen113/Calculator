9767c0b42ae26983ce1ff4434a4391aa1e57f6b2
Johnathan <johnathan.nguyen1999@gmail.com>
2020-06-14 13:23:23 -0400

    full calculator
    calculator works with background

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+const screen = document.getElementById('screen');
+const back = document.querySelector('.back');
+
+back.addEventListener('click', function(){
+    screen.innerHTML = screen.innerHTML.substring(0,screen.innerHTML.length-1);
+});
+
+function pushBtn(obj){
+    //assigning pushed values to the screen, this doesn't print it on the screen yet
+    var pushed = obj.innerHTML;
+    if(pushed === '='){
+        screen.innerHTML = eval(screen.innerHTML);
+    }else if(pushed === 'A/C'){
+        screen.innerHTML = '0';
+    }else{
+        if(screen.innerHTML === '0'){
+            //replace 0 with the value being pushed
+            screen.innerHTML = pushed;
+        }else{
+            //adds on elements to the screen whenever pushed
+            screen.innerHTML += pushed;
+        }
+    }
+}
+
+
+
 
